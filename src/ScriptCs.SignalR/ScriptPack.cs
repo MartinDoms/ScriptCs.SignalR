3b16d80ed119df9da40e2118bba221ad9723ee41
Martin Doms <iamaelephant@gmail.com>
2013-05-09 20:55:33 +1200

    Implemented server as script pack

---
+++ src/ScriptCs.SignalR/ScriptPack.cs
@@ -0,0 +1,21 @@
+﻿using ScriptCs.Contracts;
+
+namespace ScriptCs.SignalR
+{
+    public class ScriptPack : IScriptPack
+    {
+        IScriptPackContext IScriptPack.GetContext()
+        {
+            return new SignalR();
+        }
+
+        void IScriptPack.Initialize(IScriptPackSession session)
+        {
+            session.ImportNamespace("Microsoft.Owin.Hosting");
+        }
+
+        void IScriptPack.Terminate()
+        {
+        }
+    }
+}
---
+++ src/ScriptCs.SignalR/SignalR.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin.Hosting;
+using Owin;
+using ScriptCs.Contracts;
+
+namespace ScriptCs.SignalR
+{
+    public class SignalR : IScriptPackContext
+    {
+        public IDisposable CreateServer(string baseAddress)
+        {
+            return WebApplication.Start<Startup>(baseAddress);
+        }
+
+        public IDisposable CreateServer()
+        {
+            return WebApplication.Start<Startup>("http://localhost:8080");
+        }
+
+        public IDisposable CreateServer<TStartup>(string baseAddress)
+        {
+            return WebApplication.Start<TStartup>();            
+        }
+
+        public IDisposable CreateServer<TStartup>()
+        {
+            return WebApplication.Start<TStartup>("http://localhost:8080");
+        }
+    }
+
+    internal class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HubConfiguration { EnableCrossDomain = true };
+            app.MapHubs(config);
+        }
+    }
+}

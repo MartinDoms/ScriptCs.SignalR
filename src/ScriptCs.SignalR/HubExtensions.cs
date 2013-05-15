dbc3aa0715423831b353c6cc5f76b4b59269ff0c
unknown <martind@WKS-0955.intergen.org.nz>
2013-05-15 13:23:28 +1200

    Now using Invoke() instead of callsite reflection for calling hub members

--- src/ScriptCs.SignalR/HubExtensions.cs
+++ src/ScriptCs.SignalR/HubExtensions.cs
@@ -1,32 +1,28 @@
-﻿using System;
-using System.Dynamic;
-using System.Linq;
-using System.Runtime.CompilerServices;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNet.SignalR.Hubs;
-using Microsoft.CSharp.RuntimeBinder;
 
 namespace ScriptCs.SignalR
 {
     public static class HubExtensions
     {
-        public static void SendToAll(this HubConnectionContext hub, string messageName, object message)
+        public static Task SendToAll(this HubConnectionContext hub, string messageName, object message)
         {
-            SendMessage(hub.All, messageName, message);
+            return SendMessage(hub.All, messageName, message);
         }
 
-        public static void SendToCaller(this HubConnectionContext hub, string messageName, object message)
+        public static Task SendToCaller(this HubConnectionContext hub, string messageName, object message)
         {
-            SendMessage(hub.Caller, messageName, message);
+            return SendMessage(hub.Caller, messageName, message);
         }
 
-        private static void SendMessage(dynamic obj, string messageName, object message)
+        private static Task SendMessage(dynamic obj, string messageName, object message)
         {
-            var dynamicObject = obj as DynamicObject;
-            var callSiteBinder = Binder.InvokeMember(CSharpBinderFlags.None, messageName, Enumerable.Empty<Type>(), typeof(HubConnectionContext),
-                                    new[] { CSharpArgumentInfo.Create(CSharpArgumentInfoFlags.None, null), 
-                                            CSharpArgumentInfo.Create(CSharpArgumentInfoFlags.None, null) });
-            var callSite = CallSite<Action<CallSite, object, object>>.Create(callSiteBinder);
-            callSite.Target(callSite, dynamicObject, message);
+            var proxy = obj as IClientProxy;
+            if (proxy != null)
+            {
+                return proxy.Invoke(messageName, message);
+            }
+            return null;
         }
     }
 }
